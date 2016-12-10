be262a69b6708d968ed8fe5b41b777ae0e74ab93
bm <tohle12@gmail.com>
2016-12-09 23:06:11 -0500

    intial commit

---
+++ app-questions.js
@@ -0,0 +1,30 @@
+module.exports = [
+    {
+        "q": "Food",
+        "a": "FoodBizzare by Joe",
+        "b": "Bodega by Maria",
+        "c": "Food R Us by Mike",
+        "d": "Farmer's Market by Mark "
+    },
+    {
+        "q": "Style",
+        "a": "Hot Topic by Edison",
+        "b": "Nike Pop Up Shop by Khira",
+        "c": "Family Clothing Company by Carlos & Family",
+        "d": "Rainbow Inc"
+    },
+    {
+        "q": "Health",
+        "a": "Rite Aid Inc",
+        "b": "Pharmacy by Ms.Max",
+        "c": "Walgreens Inc",
+        "d": "Duane Reade Inc"
+    },
+    {
+        "q": "Handy Work",
+        "a": "99 cent Store",
+        "b": "Hardware Shop",
+        "c": "Locksmith",
+        "d": "Small Gym"
+    }
+];
